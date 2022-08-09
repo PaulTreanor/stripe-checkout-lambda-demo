15df04c84a5dbf32dc64c721a0c9f440a05daf5a
paultreanor <paul.treanor@fourtheorem.com>
2022-08-09 14:50:35 +0100

    Add product card component

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="../assets/apple.jpg" alt="Sunset in the mountains">
+      <div class="px-7 font-bold text-xl pb-5">{{name}}</div>
+      <span class="mx-6 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{{price}}</span>
+
+      <div class="px-6 pb-2">
+    
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Buy me</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['name', 'price']
+}
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<script>
+import Card from '../components/Card.vue'
+export default { 
+  components: {
+    Card
+  }, 
+  data() {
+    return {
+      products: [
+        {
+          name: 'Apple',
+          price: '$1.00'
+        },
+        {
+          name: 'Banana',
+          price: '$2.00'
+        },
+        {
+          name: 'Orange',
+          price: '$3.00'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<template>
+  <main>
+    <h1>Stripe checkout demo app</h1>
+    <Card :name="products[0].name" :price="products[0].price"/>
+  </main>
+</template>
